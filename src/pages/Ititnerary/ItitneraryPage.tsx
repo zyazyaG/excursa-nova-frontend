73efcca6a560df8f6dc955ca64d3fa6e3ea3a833
Aziza Gulyamova <agulyamova14@gmail.com>
2025-05-06 15:44:33 -0400

    add Printing option

--- src/pages/Ititnerary/ItitneraryPage.tsx
+++ src/pages/Ititnerary/ItitneraryPage.tsx
@@ -1,11 +1,12 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { Itinerary } from "../../types/itinerary"
 import styles from "./Ititnerary.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAxios } from "../../hooks/useAxios";
 import MarkdownOutput from "../../components/MarkdownOutput/MarkdownOutput";
 import { formatDate } from "../../utils";
 import Button from "../../components/Button/Button";
+import { FaPrint } from "react-icons/fa";
 
 
 export const ItitneraryPage = ()  => {
@@ -13,6 +14,7 @@ export const ItitneraryPage = ()  => {
     const [itinerary, setItinerary] = useState<Itinerary>();
     const axiosPrivate = useAxios();
     const navigator = useNavigate();
+    const printRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         let isMounted = true;
@@ -34,6 +36,18 @@ export const ItitneraryPage = ()  => {
         }
     },[_id]);
 
+    const handlePrint = () => {
+        if (printRef.current) {
+            const printContents = printRef.current.innerHTML;
+            const originalContents = document.body.innerHTML;
+    
+            document.body.innerHTML = printContents;
+            window.print();
+            document.body.innerHTML = originalContents;
+            window.location.href = window.location.href;
+        }
+    };
+
   return (
     <div className={styles.container}>
         {itinerary &&
@@ -44,42 +58,25 @@ export const ItitneraryPage = ()  => {
                     <div className={styles.left}>
                         <div className={styles.back}><Button onClick={() => navigator(-1)}>←</Button></div>
                     </div>
-                    <div className={styles.right}>
+                    <div className={styles.right} ref = {printRef}>
                         <div className={styles.header}>
                             <h2>{itinerary.name || itinerary.destination}</h2>
-                            <div className={styles.date}>
-                                <div>{formatDate(itinerary.startDate)} → {formatDate(itinerary.endDate)}</div>
-                                <div className={styles.style}>{itinerary.style}</div>
+                            <div className={styles.subHeader}>
+                                
+                                <div className={styles.date}>
+                                    <div>{formatDate(itinerary.startDate)} → {formatDate(itinerary.endDate)}</div>
+                                    <div className={styles.style}>{itinerary.style}</div>
+                                </div>
+                                <div onClick={handlePrint}><FaPrint /></div>
                             </div>
                         </div>
                         <div className={styles.content}>
                             <MarkdownOutput markdown={itinerary.content} />      
                         </div>
-
-
                     </div>
                 </div>
             </div>
         }
-
-
-
-
-
-{/* \        <div className={styles.inner}>
-            {itinerary && 
-                <div className={styles.itinerary}>
-                    <div className={styles.header}>
-                        <h2>{itinerary.name}</h2>
-                    </div>
-                    <div className={styles.image}><img src={itinerary.imgURL[0]} alt="Image from Unsplash" title={itinerary.imgURL[2]}/></div>
-                    <div>
-                    <MarkdownOutput markdown={itinerary.content} />
-                    </div>
-                </div>
-            }
-            
-        </div> */}
 
     </div>
   )
